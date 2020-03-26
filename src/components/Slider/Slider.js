2a06be3ba2fd9d371b6b775ced21dbac09683046
nibir <biplob.asanibir@gmail.com>
2020-03-26 09:56:46 +0600

    search add

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Slider.css'
 
 const Slider = () => {
     return (
         <div>
-            <Container fluid>
-                <Row>
-                <div className="search-container">
-                    <h1>Best food waiting for your belly</h1>
-                    <span>
-                        <input className="" type="text" placeholder="Search food items"/>
-                        <button className="round-button">Search</button>
-                    </span>
-                </div>
+            <Container fluid className="search-container">
+                <Row className="mb-4">      
+                    <Col> <h1 className="d-flex justify-content-center align-items-center">Best food waiting for your belly</h1></Col>
+                </Row>
+                <Row >
+                   <Col md={{ span: 4, offset: 4 }}>
+                         <span className="justify-content-md-center">                        
+                            <div className="input-group w-8 search-input">
+                                <input type="text" className="form-control search" placeholder="Search food items" />
+                                <div className="input-group-append">
+                                    <button className="round-button search-btn" type="button">Search</button>
+                                </div>
+                            </div>
+                        </span>
+                   </Col>
                 </Row>
             </Container>
         </div>
